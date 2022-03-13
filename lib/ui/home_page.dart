7dda64feab507f69c5b4009b1fb3fd146490ecd1
Paulo Roberto <paulo_correntinho@hotmail.com>
2022-03-13 11:30:36 -0300

    atualização

--- lib/ui/home_page.dart
+++ lib/ui/home_page.dart
@@ -29,6 +29,14 @@ class _HomePageState extends State<HomePage> {
   }
 
   @override
+  void initState() {
+    // TODO: implement initState
+    setState(() {
+      print(_getGifs());
+    });
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
@@ -39,68 +47,68 @@ class _HomePageState extends State<HomePage> {
       ),
       backgroundColor: Colors.black,
       body: Column(
-        children: [
-          const Padding(
-            padding: EdgeInsets.all(10.0),
-            child: TextField(
-              decoration: InputDecoration(
-                labelText: "Pesquise Aqui!!!",
-                labelStyle: TextStyle(color: Colors.white),
-                border: OutlineInputBorder(),
+          children: [
+            Padding(
+              padding: EdgeInsets.all(10.0),
+              child: TextField(
+                decoration: const InputDecoration(
+                  labelText: "Pesquise Aqui!!!",
+                  labelStyle: TextStyle(color: Colors.white),
+                  border: OutlineInputBorder(),
+                ),
+                style: const TextStyle(color: Colors.white, fontSize: 18.0),
+                textAlign: TextAlign.center,
+                onSubmitted: (text) {
+                  setState(() {
+                    _search = text;
+                  });
+                },
               ),
-              style: TextStyle(color: Colors.white, fontSize: 18.0),
-              textAlign: TextAlign.center,
-              onSubmitted: (text) {     //pesquisa o texto digitado
-                setState(() {
-                  _search = text;
-                });
-              },
             ),
-          ),
-          Expanded(
-            child: FutureBuilder(
-                future: _getGifs(),
-                builder: (context, snapshot) {
-                  switch (snapshot.connectionState) {
-                    case ConnectionState.waiting:
-                    case ConnectionState.none:
-                      return Container(
-                        width: 200.0,
-                        height: 200.0,
-                        alignment: Alignment.center,
-                        child: const CircularProgressIndicator(
-                          valueColor: AlwaysStoppedAnimation<Color>(
-                              Colors.white),
-                          strokeWidth: 5.0,
-                        ),
-                      );
-                    default:
-                      if (snapshot.hasError)
-                        return Container();
-                      else {
-                        return _createGifTable(context, snapshot);
-                      }
+            Expanded(
+              child: FutureBuilder(
+                  future: _getGifs(),
+                  builder: (context, snapshot) {
+                    switch (snapshot.connectionState) {
+                      case ConnectionState.waiting:
+                      case ConnectionState.none:
+                        return Container(
+                          width: 200.0,
+                          height: 200.0,
+                          alignment: Alignment.center,
+                          child: const CircularProgressIndicator(
+                            valueColor: AlwaysStoppedAnimation<Color>(
+                                Colors.white),
+                            strokeWidth: 5.0,
+                          ),
+                        );
+                      default:
+                        if (snapshot.hasError)
+                          return Container();
+                        else {
+                          return _createGifTable(context, snapshot);
+                        }
+                    }
                   }
-                }
+              ),
             ),
-          ),
-        ]
+          ]
       ),
     );
   }
 
   Widget _createGifTable(BuildContext context, AsyncSnapshot snapshot) {     //cria uma grade de itens
     return GridView.builder(
-      padding:const EdgeInsets.all(10.0),
+        padding:const EdgeInsets.all(10.0),
         gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-          crossAxisCount: 2,     //quantidade de itens na vertical
-          crossAxisSpacing: 10.0,     //espacamento entre os itens
-          mainAxisSpacing: 10.0     //espacamento na vertical
+            crossAxisCount: 2,     //quantidade de itens na vertical
+            crossAxisSpacing: 10.0,     //espacamento entre os itens
+            mainAxisSpacing: 10.0     //espacamento na vertical
         ),
         itemCount: snapshot.data["data"].length,     //quantidade de itens na horizontal
         itemBuilder: (context, index) {
           return GestureDetector(
-            child: Image.network(snapshot.data["data"][index]["imagens"]["fixed_height"]["url"],
+            child: Image.network(snapshot.data["data"][index]["images"]["fixed_height"]["url"],
               height: 300.0,
               fit: BoxFit.cover,
             ),
